624193763f3ed24bb063a0cd958c6ad8c3004756
Millan Madrid <millan.madrid@unir.net>
2026-06-05 18:28:03 +0200

    feat: centralizar datos del equipo nº 10 y actualizar footer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './About.css'
+import { TEAM_NUMBER } from '../data/team'
 
 const About = () => {
     
@@ -7,7 +8,7 @@ const About = () => {
     <div className="about-container">
         <h1>Acerca de nosotros</h1>
         <div id="info">
-            Somos el equipo nº 23
+            Somos el equipo nº {TEAM_NUMBER}
         </div>
     </div>
 
